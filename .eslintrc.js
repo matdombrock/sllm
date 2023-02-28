0de3db23262b0eccf9192d92dc972c5cf975d306
Mathieu Dombrock <matdombrock@gmail.com>
2023-02-27 19:27:35 -0800

    fix eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
