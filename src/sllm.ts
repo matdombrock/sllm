5a2a0b74552d82c475ef0ccf0bd41078895c6272
Mathieu Dombrock <matdombrock@gmail.com>
2023-05-05 00:07:51 -0700

    full ts

--- src/sllm.ts
+++ src/sllm.ts
@@ -5,16 +5,13 @@ import os from 'os';
 import { encode } from 'gpt-3-encoder';
 import { Configuration, CreateChatCompletionRequest, OpenAIApi } from 'openai';
 
-import { modelMap, modelAlias } from './modelMap.js';
+import { modelMap, modelAlias, ModelInfo } from './models.js';
 
 const USER_CFG_DIR: string = os.homedir() + '/.config/sllm';
 
 const MAX_HISTORY_STORE: number = 64;
 
-// Ensure we have an api key env var
-_ensureAPIKey();
-// Ensure we have our needed files
-_ensureFiles();
+
 
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
@@ -24,6 +21,12 @@ const openai = new OpenAIApi(configuration);
 
 
 class SLLM {
+	constructor(){
+		// Ensure we have an api key env var
+		this.ensureAPIKey();
+		// Ensure we have our needed files
+		this.ensureFiles();
+	}
 	/*
 		Main Prompt Function
 	*/
@@ -32,7 +35,7 @@ class SLLM {
 		let prompt: string = promptArr.join(' ');
 	
 		// Set static options
-		options = _loadOpts(options);
+		options = this.loadOpts(options);
 	
 		// Ensure numbers
 		options.maxTokens = Number(options.maxTokens) || 256;
@@ -41,7 +44,7 @@ class SLLM {
 	
 		// Prepend a file if required
 		if (options.file) {
-			prompt = _loadFile(prompt, options);
+			prompt = this.loadFile(prompt, options);
 		}
 		// Check model aliases
 		if(modelAlias[options.model]){
@@ -54,22 +57,22 @@ class SLLM {
 		}
 		// Preprocess the prompt
 		if (options.likeImFive) {
-			prompt = _preLike5(prompt);
+			prompt = this.preLike5(prompt);
 		}
 		if (options.context) {
-			prompt = _preContext(prompt, options.context);
+			prompt = this.preContext(prompt, options.context);
 		}
 		if (options.domain) {
-			prompt = _preDomain(prompt, options.domain);
+			prompt = this.preDomain(prompt, options.domain);
 		}
 		if (options.expert) {
-			prompt = _preExpert(prompt, options.expert);
+			prompt = this.preExpert(prompt, options.expert);
 		}
 	
 		// Append History
 		const ogPrompt = prompt; // Cache for history etc
 		if (options.history) {
-			prompt =_loadHistory(options.history, false) + prompt; // Reversed
+			prompt =this.loadHistory(options.history, false) + prompt; // Reversed
 		}
 	
 		// Count total prompt tokens and append to the maxTokens value
@@ -85,10 +88,10 @@ class SLLM {
 	
 		// Apply verbose output
 		if (options.verbose) {
-			_verbose(ogPrompt, options, encoded, totalTokens);
+			this.verbose(ogPrompt, options, encoded, totalTokens);
 		}
 	
-		if(_overTokenLimit(totalTokens, modelData, options)){return;}
+		if(this.overTokenLimit(totalTokens, modelData, options)){return;}
 	
 		// Make the request
 		let output = 'WARNING: Did not send!';
@@ -123,7 +126,7 @@ class SLLM {
 		console.log(output);
 	
 		// Log history
-		_logHistory(ogPrompt, output);
+		this.logHistory(ogPrompt, output);
 	}
 	/*
 		Command functions
@@ -131,7 +134,7 @@ class SLLM {
 	public historyView(options: any): void {
 		// Ensure number
 		options.number = Number(options.view) || 32;
-		const content = _loadHistory(options.view, false);
+		const content = this.loadHistory(options.view, false);
 		console.log(content);
 		return;
 	}
@@ -157,7 +160,7 @@ class SLLM {
 		console.log("History undone!");
 	}
 	public repeat(): void {
-		const last = _loadHistory(1, true, true);
+		const last = this.loadHistory(1, true, true);
 		console.log(last[0].llm);
 	}
 	public settingsView(): void {
@@ -201,7 +204,7 @@ class SLLM {
 		if (options.file) {
 			let fileContents = fs.readFileSync(options.file, 'utf-8');
 			if(options.trim){
-				fileContents = _trim(fileContents);
+				fileContents = this.trim(fileContents);
 			}
 			const encoded = encode(fileContents);
 			tokens += encoded.length;
@@ -292,161 +295,152 @@ class SLLM {
 		});
 		return completion?.data?.choices[0]?.message?.content || "No response";
 	}
+	/*
+	Prompt Pre-processing
+	*/
+	private preLike5(prompt:string):string{
+		return 'Answer this as if I\'m five years old: ' + prompt;
+	}
+	private preContext(prompt:string, context:Array<string>):string{
+		return 'In the context of ' + context.join(' ') + ', ' + prompt;
+	}
+	private preDomain(prompt:string, domain:Array<string>):string{
+		return 'In the domain of ' + domain.join(' ') + ', ' + prompt;
+	}
+	private preExpert(prompt:string, expert:Array<string>):string{
+		return 'Act as an expert on ' +
+				expert.join(' ') +
+				'. The question I need you to answer is: ' +
+				prompt;
+	}
+	/*
+	Utility Functions
+	*/
+	// Handle verbose logging
+	private verbose(ogPrompt:string, options:any, encoded:Array<number>, totalTokens:number):void{
+		console.log('>> ' + ogPrompt + ' <<');
+		console.log(JSON.stringify(options, null, 2));
+		console.log('Encoded Tokens: ' + encoded.length);
+		console.log('Total Potential Tokens: ' + totalTokens);
+		console.log('Sending Prompt...');
+		console.log('-------\r\n');
+	}
+	// Load a file from th FS
+	// Handle trim if specified
+	private loadFile(prompt:string, options):string{
+		let fileContents = '';
+		const fileLoc = options.file;
+		if (fs.existsSync(fileLoc)) {
+			fileContents = fs.readFileSync(fileLoc, 'utf-8');
+		}
+		if(options.trim){
+			fileContents = this.trim(fileContents);
+		}
+		prompt ='```\r\n' +
+		fileContents +
+		'```\r\n' +
+		prompt;
+		return prompt;
+	}
+	// Check if we are over the token limit
+	// Return true if we are over the token limit
+	private overTokenLimit(totalTokens:number, modelData:ModelInfo, options):boolean{
+		if (options.maxTokens > modelData.maxTokens){
+			console.log(`ERROR: You requested ${options.maxTokens} which exceeds the model limit of ${modelData.maxTokens}`);
+			return true;
+		}
+		if (totalTokens > modelData.maxTokens - 96) {
+			console.log('ERROR: Max Tokens Exceeded ' + '(' + totalTokens + ')');
+			console.log('Please limit your prompt');
+			if (options.history) {
+				console.log('Trying running with history off!');
+			}
+			return true;
+		}
+		return false;
+	}
+	// Ensure we have the needed files 
+	// Create them if we have to
+	private ensureFiles() {
+		if (!fs.existsSync(USER_CFG_DIR)) {
+			// Create the dir
+			fs.mkdirSync(USER_CFG_DIR);
+		}
+		if (!fs.existsSync(USER_CFG_DIR + '/history.json')) {
+			// Create the file
+			fs.writeFileSync(USER_CFG_DIR + '/history.json', '[]');
+		}
+		if (!fs.existsSync(USER_CFG_DIR + '/settings.json')) {
+			// Create the file
+			fs.writeFileSync(USER_CFG_DIR + '/settings.json', '{}');
+		}
+	}
+	// Ensure we have the API key setup
+	private ensureAPIKey():void {
+		if (!process.env.OPENAI_API_KEY) {
+			let err = 'ERROR: OPENAI_API_KEY unset\r\n';
+			err += 'To set, use the command:\r\n';
+			err += 'export OPENAI_API_KEY=<your_key>\r\n';
+			err += 'https://platform.openai.com/account/api-keys';
+			console.log(err);
+			process.exit();
+		}
+	}
+	// Overwrite the CLI options with those saved in the file
+	private loadOpts(options:any):any {
+		const content = fs.readFileSync(USER_CFG_DIR + '/settings.json', 'utf-8');
+		const optJSON = JSON.parse(content);
+		options = Object.assign(optJSON, options);
+		return options;
+	}
+	// Record the chat history to the local log
+	private logHistory(ogPrompt:string, output:string):void {
+		const histNew = {
+			user: ogPrompt,
+			llm: output,
+		};
+		const historyJSON = this.loadHistory(MAX_HISTORY_STORE, false, true);
+		historyJSON.push(histNew);
+		fs.writeFileSync(
+			USER_CFG_DIR + '/history.json',
+			JSON.stringify(historyJSON, null, 2)
+		);
+	}
+	// Load history from the local log
+	private loadHistory(count:number = 1, reverse:boolean = true, json:boolean = false):any {// Really json or string
+		let content = fs.readFileSync(USER_CFG_DIR + '/history.json', 'utf-8');
+		if (!content) {
+			console.log('WARNING: Can not read history file!');
+			content = '[]';
+		}
+		let historyJSON = JSON.parse(content) || [];
+		// Always reverse history first
+		// Reverse chronological
+		historyJSON.reverse();
+		// Slice the history
+		historyJSON = historyJSON.slice(0, count);
+		// Undo reverse if needed
+		if (reverse === false) {
+			historyJSON.reverse();
+		}
+		if (json) {
+			// Return json
+			return historyJSON;
+		}
+		let historyStr = '';
+		for (const item of historyJSON) {
+			historyStr += '_user_: ' + item.user;
+			historyStr += '\r\n';
+			historyStr += '_llm_: ' + item.llm;
+			historyStr += '\r\n';
+		}
+		return historyStr;
+	}
+
+	private trim(str:string):string{
+		return str.replace((/  |\r\n|\n|\r/gm),"");
+	}
+	
 };
 
 export default SLLM;
-
-/*
-	Prompt Pre-processing
-*/
-function _preLike5(prompt){
-	return 'Answer this as if I\'m five years old: ' + prompt;
-}
-function _preContext(prompt, context){
-	return 'In the context of ' + context.join(' ') + ', ' + prompt;
-}
-function _preDomain(prompt, domain){
-	return 'In the domain of ' + domain.join(' ') + ', ' + prompt;
-}
-function _preExpert(prompt, expert){
-	return 'Act as an expert on ' +
-			expert.join(' ') +
-			'. The question I need you to answer is: ' +
-			prompt;
-}
-
-/*
-	Utility Functions
-*/
-// Handle verbose logging
-function _verbose(ogPrompt, options, encoded, totalTokens){
-	console.log('>> ' + ogPrompt + ' <<');
-	console.log(JSON.stringify(options, null, 2));
-	console.log('Encoded Tokens: ' + encoded.length);
-	console.log('Total Potential Tokens: ' + totalTokens);
-	console.log('Sending Prompt...');
-	console.log('-------\r\n');
-}
-
-// Load a file from th FS
-// Handle trim if specified
-function _loadFile(prompt:string, options){
-	let fileContents = '';
-	const fileLoc = options.file;
-	if (fs.existsSync(fileLoc)) {
-		fileContents = fs.readFileSync(fileLoc, 'utf-8');
-	}
-	if(options.trim){
-		fileContents = _trim(fileContents);
-	}
-	prompt ='```\r\n' +
-	fileContents +
-	'```\r\n' +
-	prompt;
-	return prompt;
-}
-function _trim(str){
-	return str.replace((/  |\r\n|\n|\r/gm),"");
-}
-
-// Check if we are over the token limit
-// Return true if we are over the token limit
-function _overTokenLimit(totalTokens, modelData, options){
-	if (options.maxTokens > modelData.maxTokens){
-		console.log(`ERROR: You requested ${options.maxTokens} which exceeds the model limit of ${modelData.maxTokens}`);
-		return true;
-	}
-	if (totalTokens > modelData.maxTokens - 96) {
-		console.log('ERROR: Max Tokens Exceeded ' + '(' + totalTokens + ')');
-		console.log('Please limit your prompt');
-		if (options.history) {
-			console.log('Trying running with history off!');
-		}
-		return true;
-	}
-	return false;
-}
-
-// Ensure we have the needed files 
-// Create them if we have to
-function _ensureFiles() {
-	if (!fs.existsSync(USER_CFG_DIR)) {
-		// Create the dir
-		fs.mkdirSync(USER_CFG_DIR);
-	}
-	if (!fs.existsSync(USER_CFG_DIR + '/history.json')) {
-		// Create the file
-		fs.writeFileSync(USER_CFG_DIR + '/history.json', '[]');
-	}
-	if (!fs.existsSync(USER_CFG_DIR + '/settings.json')) {
-		// Create the file
-		fs.writeFileSync(USER_CFG_DIR + '/settings.json', '{}');
-	}
-}
-
-// Ensure we have the API key setup
-function _ensureAPIKey() {
-	if (!process.env.OPENAI_API_KEY) {
-		let err = 'ERROR: OPENAI_API_KEY unset\r\n';
-		err += 'To set, use the command:\r\n';
-		err += 'export OPENAI_API_KEY=<your_key>\r\n';
-		err += 'https://platform.openai.com/account/api-keys';
-		console.log(err);
-		process.exit();
-	}
-}
-
-// Overwrite the CLI options with those saved in the file
-function _loadOpts(options) {
-	const content = fs.readFileSync(USER_CFG_DIR + '/settings.json', 'utf-8');
-	const optJSON = JSON.parse(content);
-	options = Object.assign(optJSON, options);
-	return options;
-}
-
-// Record the chat history to the local log
-function _logHistory(ogPrompt, output) {
-	const histNew = {
-		user: ogPrompt,
-		llm: output,
-	};
-	const historyJSON = _loadHistory(MAX_HISTORY_STORE, false, true);
-	historyJSON.push(histNew);
-	fs.writeFileSync(
-		USER_CFG_DIR + '/history.json',
-		JSON.stringify(historyJSON, null, 2)
-	);
-}
-
-// Load history from the local log
-function _loadHistory(count = 1, reverse = true, json = false) {
-	let content = fs.readFileSync(USER_CFG_DIR + '/history.json', 'utf-8');
-	if (!content) {
-		console.log('WARNING: Can not read history file!');
-		content = '[]';
-	}
-	let historyJSON = JSON.parse(content) || [];
-	// Always reverse history first
-	// Reverse chronological
-	historyJSON.reverse();
-	// Slice the history
-	historyJSON = historyJSON.slice(0, count);
-	// Undo reverse if needed
-	if (reverse === false) {
-		historyJSON.reverse();
-	}
-	if (json) {
-		// Return json
-		return historyJSON;
-	}
-	let historyStr = '';
-	for (const item of historyJSON) {
-		historyStr += '_user_: ' + item.user;
-		historyStr += '\r\n';
-		historyStr += '_llm_: ' + item.llm;
-		historyStr += '\r\n';
-	}
-	return historyStr;
-}
-
-
